6c9b1c5d9f3cd69eeccc65d4e7dc6bfa6e50b84d
amit <amit@wibidata.com>
2013-06-24 22:16:26 -0700

    Fix the bulk import script.

--- run_bulk_importer.sh
+++ run_bulk_importer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export KIJI_CLASSPATH=/Users/amit/Documents/misc_workspace/kiji_mahout/target/*
+export KIJI_CLASSPATH=$PWD/target/*
 #kiji bulk-import --importer="org.hokiesuns.kiji.RatingsBulkLoader" --input="format=text file=./input/ch4/gl_10m.csv" --output="format=kiji nsplits=1 table=kiji://.env/default/product_ratings file=./hfiles"
 kiji bulk-import --importer="org.hokiesuns.kiji.RatingsBulkLoader" --input="format=text file=./input/ch2/gl_100k.csv" --output="format=kiji nsplits=1 table=kiji://.env/default/product_ratings file=./hfiles"
 #kiji bulk-import --importer="org.hokiesuns.kiji.RatingsBulkLoader" --input="format=text file=./input/ch2/input_sample.csv" --output="format=kiji nsplits=1 table=kiji://.env/default/product_ratings file=./hfiles"
